78eaf856b5bd51865555603804a921f1cc9a642e
sim <ironman1024@naver.com>
2019-11-15 15:17:22 +0900

    added pirnt line

--- Imformation/src/Imformation.java
+++ Imformation/src/Imformation.java
@@ -3,6 +3,7 @@ public class Imformation {
 
 	public static void main(String[] args) {
 		System.out.println("computer SW");
+		System.out.println("Wonkwang Univ");
 		// TODO Auto-generated method stub
 
 	}
