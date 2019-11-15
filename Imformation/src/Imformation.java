3fdf47a6cca4666103cb10b4227ee6f99d6dbde5
sim <ironman1024@naver.com>
2019-11-15 15:22:01 +0900

    hotfix branch change a code

--- Imformation/src/Imformation.java
+++ Imformation/src/Imformation.java
@@ -4,6 +4,7 @@ public class Imformation {
 	public static void main(String[] args) {
 		System.out.println("computer SW");
 		System.out.println("Wonkwang Univ");
+		System.out.println("Number (20183379)");
 		// TODO Auto-generated method stub
 
 	}
